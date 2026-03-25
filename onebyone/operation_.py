4652ecd3d956ed648dff906f7ab18eb52910c138
alphaadarsh <alphaadarsh88@gmail.com>
2026-03-25 09:08:04 +0530

    updated

---
+++ onebyone/operation_.py
@@ -0,0 +1,13 @@
+s1  =  "python is a great language"
+
+# print (s1)
+print(s1*3) # this will print the string s1 three times in a row without any space in between
+
+# membership operator in python is used to check if a substring is present in a string or not
+
+s2 = "python is fun"
+print("python" in s2)  # this will return True because the substring "python" is present in the string s2
+print("java" in s2)  # this will return False because the substring "java
+
+# comparision of string in python is done using the ascii values of the characters in the string
+ 
