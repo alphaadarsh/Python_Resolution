f581b080fc85dbb35ed4eefa530a7baf6c148dfb
alphaadarsh <alphaadarsh88@gmail.com>
2026-03-25 19:33:03 +0530

    updated

---
+++ onebyone/string_op.py
@@ -0,0 +1,10 @@
+# count function substring from  a string 
+
+# count function is used to count the number of occurrences of a substring in a string 
+name = "python is a great language and python is easy to learn"
+
+print(name.count("python"))  # this will count the number of occurrences of the substring "python" in the string name and print 2 because the substring "python" is present 2 times in the string name
+
+# also 
+
+print(f"the number of occurrences of the substring 'python' in the string name is : {name.count('python')}")  # this will print the number of occurrences of the substring "python" in the string name using f-string formatting
