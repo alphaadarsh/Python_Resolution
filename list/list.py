d76375b5918bbb9e14e021126557b2a72817e6ba
alphaadarsh <alphaadarsh88@gmail.com>
2026-03-25 21:19:10 +0530

    updated

--- list/list.py
+++ list/list.py
@@ -7,3 +7,8 @@ print(type(st_data))  # this will print the type of st_data which is <class 'lis
 print(st_data)  # this will print the list st_data which contains the name, age and percentage of a student itcan contain in any data types as allowed in elements of a list in python  in a ordered manner and it is mutable data type in python which means we can change the values of the list after creating it
 
 
+#  list is  same as indexing and slicing in string but the only difference is that list is mutable data type in python which means we can change the values of the list after creating it but string is immutable data type in python which means we cannot change the values of the string after creating it  
+
+day_in_week = ["monday" , "tuesday" , "wednesday" , "thursday" , "friday" , "saturday" , "sunday"]
+print(day_in_week)  # this will print the list day_in_week which contains the names
+print(day_in_week [2])  # this will print the list day_in_week which contains the names
