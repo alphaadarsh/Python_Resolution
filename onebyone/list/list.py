133aecd5ae09b413a34c1958648e844b9525507e
alphaadarsh <alphaadarsh88@gmail.com>
2026-03-25 19:52:05 +0530

    updated

---
+++ onebyone/list/list.py
@@ -0,0 +1,9 @@
+# list is a collection of data type  you can store multiple values in a list and it is ordered and mutable data type in python 
+
+# creating a list in python  
+
+st_data = ["Jhon" , 20 , 85.5 ]
+print(type(st_data))  # this will print the type of st_data which is <class 'list'> because st_data is a list in python
+print(st_data)  # this will print the list st_data which contains the name, age and percentage of a student itcan contain in any data types as allowed in elements of a list in python  in a ordered manner and it is mutable data type in python which means we can change the values of the list after creating it
+
+
