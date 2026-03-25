0b90da389bf326b084789edbdcbc675c18853ae4
alphaadarsh <alphaadarsh88@gmail.com>
2026-03-25 19:37:49 +0530

    updated

--- onebyone/string_op.py
+++ onebyone/string_op.py
@@ -7,4 +7,10 @@ print(name.count("python"))  # this will count the number of occurrences of the 
 
 # also 
 
-print(f"the number of occurrences of the substring 'python' in the string name is : {name.count('python')}")  # this will print the number of occurrences of the substring "python" in the string name using f-string formatting
+print(f"the number of occurrences of the substring 'python' in the string name is : {name.count('python' , 0, len(name))}")  # this will print the number of occurrences of the substring "python" in the string name using f-string formatting 
+
+#  change case of  a string  
+
+# upeer case , lower case , title case , capitalize case  # we can convert we use to .  upper() , .lower() , .title() , .capitalize() methods to change the case of a string
+
+# string.startswith(substring) # this method is used to check if a string starts with a specific substring or not and it returns True if the string starts with the specified substring and False otherwise
