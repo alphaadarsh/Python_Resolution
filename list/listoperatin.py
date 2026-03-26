a08adb76ebb334464a377cf93c2bfa689ec082db
alphaadarsh <alphaadarsh88@gmail.com>
2026-03-26 08:43:05 +0530

    updated

---
+++ list/listoperatin.py
@@ -0,0 +1,8 @@
+# reverse ,  sort , count  , Membership op , index 
+
+days_of_week = ["Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday" , "Saturday" , "Sunday"] 
+print (days_of_week)  # printing the original list of days of the week
+
+#reverse  for reversing the order of the items in the list and syntax is list_name.reverse()  # this will reverse the order of the items in the list and it will update the list with the reversed order of items
+days_of_week.reverse()  # reversing the order of the items in the list using reverse method
+print(days_of_week)  # printing the reversed list of days of the week
