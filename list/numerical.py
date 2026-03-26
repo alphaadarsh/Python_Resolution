3da7a06dd7016d5a39417029109f9122b6a0f38e
alphaadarsh <alphaadarsh88@gmail.com>
2026-03-26 10:38:41 +0530

    updated

---
+++ list/numerical.py
@@ -0,0 +1,8 @@
+number   =  [ 10 , 4  , 5.5 , 2 , 8 , 6 , 1 , 3 , 9 , 7 ]
+# smallest number in the list 
+
+
+
+smallest = min(number)  # finding the smallest number in the list using min function 
+print("Smallest number in the list: " , smallest)  # printing the smallest number in the listn7j
+print(f"Smallest number in the list: {smallest}")  # printing the smallest number in the list using f-string
