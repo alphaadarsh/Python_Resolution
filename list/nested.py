b166505777db47e62df7e26c1ccaa34c6d702bed
alphaadarsh <alphaadarsh88@gmail.com>
2026-03-29 21:18:33 +0530

    updated

---
+++ list/nested.py
@@ -0,0 +1,7 @@
+# list  inside  a list  
+
+l1 = [1, 2, 3 , 5.15 , "python" , [4, 5, 6] ]
+
+#  print(len[l1])  # this will print the length of the list l1 which is 6 because there are 6 elements in the list l1 which are 1, 2, 3, 5.15, "python" and [4, 5, 6]
+#   you can change index    
+print(l1[-1][0])  # this will print the length of the list l1 which is 6 because there are 6 elements in the list l1 which are 1, 2, 3, 5.15, "python" and [4, 5, 6]
